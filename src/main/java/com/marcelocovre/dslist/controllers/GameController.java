7d7a8605d0d3bf264fee8155d794f2d8c1edac1b
marcelo covre <marcelocovre@outlook.com>
2023-05-08 22:31:01 -0300

    Game repository, service, controller

---
+++ src/main/java/com/marcelocovre/dslist/controllers/GameController.java
@@ -0,0 +1,24 @@
+package com.marcelocovre.dslist.controllers;
+
+import com.marcelocovre.dslist.dto.GameMinDTO;
+import com.marcelocovre.dslist.services.GameService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/games")
+public class GameController {
+
+    @Autowired
+    private GameService gameService;
+
+    @GetMapping
+    public List<GameMinDTO> findAll() {
+        List<GameMinDTO> result = gameService.findAll();
+        return result;
+    }
+}
