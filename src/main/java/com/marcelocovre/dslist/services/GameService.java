323e2f95d9b08bb1cf1c5a6f66c7b0d1031ae69b
marcelo covre <marcelocovre@outlook.com>
2023-05-09 22:04:12 -0300

    SQL, projection, get games by list

--- src/main/java/com/marcelocovre/dslist/services/GameService.java
+++ src/main/java/com/marcelocovre/dslist/services/GameService.java
@@ -3,6 +3,7 @@ package com.marcelocovre.dslist.services;
 import com.marcelocovre.dslist.dto.GameDTO;
 import com.marcelocovre.dslist.dto.GameMinDTO;
 import com.marcelocovre.dslist.entities.Game;
+import com.marcelocovre.dslist.projections.GameMinProjection;
 import com.marcelocovre.dslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -26,6 +27,12 @@ public class GameService {
     @Transactional(readOnly = true)
     public List<GameMinDTO> findAll() {
         List<Game> result = gameRepository.findAll();
+        return result.stream().map(x -> new GameMinDTO(x)).toList();
+    }
+
+    @Transactional(readOnly = true)
+    public List<GameMinDTO> findByList(Long listId) {
+        List<GameMinProjection> result = gameRepository.searchByList(listId);
         return result.stream().map(x -> new GameMinDTO(x)).toList();
     }
 }
